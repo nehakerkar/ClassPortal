aaf08ca66b8ea321e572aabe36e3290094d6b638
VINAY <frags.vinay@gmail.com>
2016-02-27 01:11:54 -0500

    Finished tests for model - user

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -11,29 +11,37 @@ class UserTest < ActiveSupport::TestCase
 
   test "user should have name" do
   	@newUser.name = " "
-  	assert_not @newUser.valid?
+  	assert_not @newUser.valid?, "User: Empty user name is being allowed"
   end
 
   test "user should have email" do
   	@newUser.email = " "
-  	assert_not @newUser.valid?
+  	assert_not @newUser.valid?, "User: Empty email is being allowed"
   end
 
   test "email should be unique" do
   	duplicate_user = @newUser.dup
-  	@newuser.save
-  	assert_not duplicate_user.valid?
+  	@newUser.save
+  	assert_not duplicate_user.valid?, "User, Duplicate Email ID is being allowed"
+  end
 
   test "user should have password" do
-  	assert_not @newUser.valid?
+  	assert_not @newUser.valid?, "User, User is being created without password"
   end
 
   test "password should be of min length 3" do
   	@newUser.password = "as"
-  	assert_not @newUser.valid?
+  	assert_not @newUser.valid?, "User: Password of user is less than minimum length"
   end
 
   test "email should match regex" do
   	@newUser.email = "12"
-  	assert_match "/\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i", @newUser.email
+  	assert_no_match "/\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i", @newUser.email, "User: Email address is in invalid format"
+  end
+
+  test "is type valid" do
+  	@newUser.type = "blah"
+  	assert_not @newUser.valid?, "User: User type is invalid"
+  end
+
 end
