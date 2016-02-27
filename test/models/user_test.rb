1a65a29d95a15d5159f410309d744b517cbcf376
VINAY <frags.vinay@gmail.com>
2016-02-27 00:59:04 -0500

    Added tests for user

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -10,6 +10,30 @@ class UserTest < ActiveSupport::TestCase
   end
 
   test "user should have name" do
-  	assert @newUser.valid?
+  	@newUser.name = " "
+  	assert_not @newUser.valid?
   end
+
+  test "user should have email" do
+  	@newUser.email = " "
+  	assert_not @newUser.valid?
+  end
+
+  test "email should be unique" do
+  	duplicate_user = @newUser.dup
+  	@newuser.save
+  	assert_not duplicate_user.valid?
+
+  test "user should have password" do
+  	assert_not @newUser.valid?
+  end
+
+  test "password should be of min length 3" do
+  	@newUser.password = "as"
+  	assert_not @newUser.valid?
+  end
+
+  test "email should match regex" do
+  	@newUser.email = "12"
+  	assert_match "/\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i", @newUser.email
 end
