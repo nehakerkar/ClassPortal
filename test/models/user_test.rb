b78e5011237c3eef5ea5cce23f0258bfc433b125
VINAY <frags.vinay@gmail.com>
2016-02-27 00:26:59 -0500

    Initial test

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -4,4 +4,11 @@ class UserTest < ActiveSupport::TestCase
   # test "the truth" do
   #   assert true
   # end
+
+  def setup
+  	@newUser = User.create()
+  end
+
+  test "user should have name" do
+  	assert @newUser.valid?
 end
